91fbd7a911d634e765be7852a5e6ad9920965994
Brian Logan <brianldj@gmail.com>
2021-05-25 09:47:33 -0700

    wip

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -28,6 +28,20 @@ class Handler extends ExceptionHandler
     ];
 
     /**
+     * @param Throwable $exception
+     *
+     * @throws Throwable
+     */
+    public function report(Throwable $exception)
+    {
+        if (app()->bound('sentry') && $this->shouldReport($exception)) {
+            app('sentry')->captureException($exception);
+        }
+
+        parent::report($exception);
+    }
+
+    /**
      * Register the exception handling callbacks for the application.
      *
      * @return void
