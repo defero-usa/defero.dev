a1d75209134d5e8d3821080d2dd2e21a7ca0f554
Brian Logan <brianldj@gmail.com>
2021-05-27 11:07:59 -0700

    updated image on the homepage

--- resources/js/Layouts/Layout.vue
+++ resources/js/Layouts/Layout.vue
@@ -38,8 +38,7 @@
                             </div>
                             <div class="mt-12 -mb-16 sm:-mb-48 lg:m-0 lg:relative">
                                 <div class="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:max-w-none lg:px-0">
-                                    <!-- Illustration taken from Lucid Illustrations: https://lucid.pixsellz.io/ -->
-                                    <img class="w-full lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-auto lg:max-w-none" src="https://tailwindui.com/img/component-images/cloud-illustration-teal-cyan.svg" alt="" />
+                                    <img class="w-full lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-auto lg:max-w-none" src="/images/code.svg" alt="" />
                                 </div>
                             </div>
                         </div>
